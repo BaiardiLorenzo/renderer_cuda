cd6f1bb448a1c85c684d9c08d462ee3e2959f6ea
loreb <lorebaia@gmail.com>
2023-10-31 15:05:13 +0100

    cuda worked v2 @TODO reformat all project

---
+++ cmake-build-release-visualstudio/CMakeFiles/renderer_cuda.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/loreb/Documents/IntellijProjects/CLionProjects/renderer_cuda/main.cu" "CMakeFiles/renderer_cuda.dir/main.cu.obj" "gcc" "CMakeFiles/renderer_cuda.dir/main.cu.obj.d"
+  "C:/Users/loreb/Documents/IntellijProjects/CLionProjects/renderer_cuda/src/renderer.cu" "CMakeFiles/renderer_cuda.dir/src/renderer.cu.obj" "gcc" "CMakeFiles/renderer_cuda.dir/src/renderer.cu.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
