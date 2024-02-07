3e171fbc822bab2ee4fab8fbf64244bbd27ac164
thomasdelmoro21 <thomas.delmoro@stud.unifi.it>
2024-02-07 19:41:03 +0100

    Completata prima parte della relazione

--- src/renderer.cu
+++ src/renderer.cu
@@ -9,6 +9,37 @@ Circle* generateCircles(std::size_t n, int width, int height, int minRadius, int
     std::uniform_int_distribution<int> pointYDistribution(1, height);
     std::uniform_int_distribution<int> radiusDistribution(minRadius, maxRadius);
 
+    for (int i = 0; i < n; i++) {
+        cv::Scalar color(colorDistribution(generator), colorDistribution(generator), colorDistribution(generator), 255);
+        cv::Point center(pointXDistribution(generator), pointYDistribution(generator));
+        int r = radiusDistribution(generator);
+        circles[i] = Circle{color, center, r};
+    }
+    return circles;
+}
+
+cv::Mat* generatePlanes(std::size_t nPlanes, Circle circles[], std::size_t nCircles) {
+    auto *planes = new cv::Mat[nPlanes];
+
+    for (int i = 0; i < nPlanes; i++) {
+        planes[i] = TRANSPARENT_MAT;
+        for (int j = 0; j < nCircles; j++) {
+            auto circle = circles[i * nCircles + j];
+            cv::circle(planes[i], circle.center, circle.r, circle.color, cv::FILLED, cv::LINE_AA);
+        }
+    }
+    return planes;
+}
+
+Circle* generateCirclesPar(std::size_t n, int width, int height, int minRadius, int maxRadius) {
+    auto* circles = new Circle[n];
+    std::mt19937 generator(std::random_device{}());
+
+    std::uniform_int_distribution<int> colorDistribution(0, 255);
+    std::uniform_int_distribution<int> pointXDistribution(1, width);
+    std::uniform_int_distribution<int> pointYDistribution(1, height);
+    std::uniform_int_distribution<int> radiusDistribution(minRadius, maxRadius);
+
 #pragma omp parallel for default(none) shared(circles, generator)
     for (int i = 0; i < n; i++) {
         cv::Scalar color(colorDistribution(generator), colorDistribution(generator), colorDistribution(generator), 255);
@@ -20,7 +51,7 @@ Circle* generateCircles(std::size_t n, int width, int height, int minRadius, int
     return circles;
 }
 
-cv::Mat* generatePlanes(std::size_t nPlanes, Circle circles[], std::size_t nCircles) {
+cv::Mat* generatePlanesPar(std::size_t nPlanes, Circle circles[], std::size_t nCircles) {
     auto *planes = new cv::Mat[nPlanes];
 
 #pragma omp parallel for default(none) shared(planes, circles) firstprivate(nPlanes, nCircles)
@@ -46,11 +77,11 @@ double sequentialRenderer(cv::Mat planes[], std::size_t nPlanes) {
     for (int i = 0; i < result.rows; i++) {
         for (int j = 0; j < result.cols; j++) {
             for (int z = 0; z < nPlanes; z++) {
-                cv::Mat *src2 = &planes[z];
+                cv::Mat *src = &planes[z];
                 for (int c = 0; c < cn; c++)
                     result.data[i * result.step + cn * j + c] =
                             result.data[i * result.step + j * cn + c] * (1 - ALPHA) +
-                            src2->data[i * src2->step + j * cn + c] * (ALPHA);
+                            src->data[i * src->step + j * cn + c] * (ALPHA);
             }
         }
     }
@@ -75,11 +106,11 @@ double parallelRenderer(cv::Mat planes[], std::size_t nPlanes) {
     for (int i = 0; i < result.rows; i++) {
         for (int j = 0; j < result.cols; j++) {
             for (int z = 0; z < nPlanes; z++) {
-                cv::Mat *src2 = &planes[z];
+                cv::Mat *src = &planes[z];
                 for (int c = 0; c < cn; c++)
                     result.data[i * result.step + cn * j + c] =
                             result.data[i * result.step + j * cn + c] * (1 - ALPHA) +
-                            src2->data[i * src2->step + j * cn + c] * (ALPHA);
+                            src->data[i * src->step + j * cn + c] * (ALPHA);
             }
         }
     }
@@ -117,7 +148,7 @@ double cudaRenderer(cv::Mat planes[], std::size_t nPlanes) {
         cudaMemcpy(d_planesData + i * width * height, planes[i].data, width * height * sizeof(uchar4), cudaMemcpyHostToDevice);
 
     // GRID AND BLOCK DIMENSIONS
-    dim3 block(4, 4); // threads x block
+    dim3 block(16, 16); // threads x block
     dim3 grid(result.cols / block.x, result.rows/ block.y); // blocks
 
     // START
